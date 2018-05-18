43ead1ed0dfa19e049ed817aaee468d5c721d106
Polychronis Ypodimatopoulos <pypodimatopoulos@linkedin.com>
2018-05-18 00:12:28 -0700

    test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
+from tornado.routing import RuleRouter, Rule, PathMatches
 import time
 import json
 # import redis
@@ -9,20 +10,35 @@ import random
 import datetime
 from operator import attrgetter, itemgetter
 
+supportedApps = ["app1", "game2"]
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self, gid=None):
-    	# self.write("ok")
-        self.render("index.html")
+        self.render("index.html", gid=gid)
+
+class DefaultHandler(tornado.web.RequestHandler):
+    def get(self, path=""):
+        self.render("404.html", path=path, supported=supportedApps)
  
 def main():
     settings = dict(template_path="html", static_path="static", debug=True)
-    application = tornado.web.Application([
-        (r"/(\d*)", MainHandler),
-        # (r'/favicon.ico', tornado.web.StaticFileHandler),
-        (r'/static/', tornado.web.StaticFileHandler),
-    ], **settings)
+    
+    rules = []
+    for appName in supportedApps:
+        app = tornado.web.Application([(r"/%s(.*)" % appName, MainHandler)], **settings)
+        rules.append(Rule(PathMatches("/%s.*" % appName), app))
 
-    http_server = tornado.httpserver.HTTPServer(application)
+    app = tornado.web.Application([(r"/(.*)", DefaultHandler)], **settings)
+    rules.append(Rule(PathMatches(r"/.*"), app))
+    router = RuleRouter(rules)
+
+    # application = tornado.web.Application([
+    #     (r"/(.*)", MainHandler),
+    #     # (r'/favicon.ico', tornado.web.StaticFileHandler),
+    #     (r'/static/', tornado.web.StaticFileHandler),
+    # ], **settings)
+
+    http_server = tornado.httpserver.HTTPServer(router)
     port = int(os.environ.get("PORT", 5000))
     http_server.listen(port)
     tornado.ioloop.IOLoop.instance().start()
