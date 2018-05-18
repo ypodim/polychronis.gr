72dd69293ef07d3a73946e2894b8712d6d550044
Polychronis Ypodimatopoulos <pypodimatopoulos@linkedin.com>
2018-05-17 22:06:21 -0700

    test

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+import time
+import json
+import redis
+import random
+import datetime
+from operator import attrgetter, itemgetter
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self, gid=None):
+    	self.write("ok")
+        # self.render("index.html")
+ 
+def main():
+    store = Store()
+    settings = dict(template_path="html", static_path="static", debug=True)
+    application = tornado.web.Application([
+        (r"/(\d*)", MainHandler, dict(store=store)),
+        # (r'/favicon.ico', tornado.web.StaticFileHandler),
+        (r'/static/', tornado.web.StaticFileHandler),
+    ], **settings)
+
+    http_server = tornado.httpserver.HTTPServer(application)
+    port = int(os.environ.get("PORT", 5000))
+    http_server.listen(port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
+
